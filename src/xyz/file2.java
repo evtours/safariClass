358537f47340c1323cc181dd12023f3c94b6ef6c
ianseguton <ianseguton@gmail.com>
2019-02-25 16:52:33 -0500

    first commit

---
+++ src/xyz/file2.java
@@ -0,0 +1,18 @@
+package xyz;
+
+public class file2 {
+    public static void main(String[] args) {
+        String [] names;
+        String output;
+        names = new String[5];
+        names[0]= "KIng";
+        names[1]= "Evans";
+        names[2] = "Edwin";
+        names[3]= "Eric";
+        names[4]= "Wanyama";
+        System.out.println(names[0]);
+
+        output = names[0]+" "+names[1]+" "+names[2]+" "+names[3]+" "+names[4];
+        System.out.println(output);
+    }
+}
---
+++ src/xyz/file3.java
@@ -0,0 +1,20 @@
+package xyz;
+
+public class file3 {
+    public static void main(String[] args) {
+        String [] wanafunzi;
+        String output;
+        wanafunzi = new String[5];
+        wanafunzi[0] = "Anthony";
+        wanafunzi[1] = "Miles";
+        wanafunzi[2] = "Peter";
+        wanafunzi[3] = "Brian";
+        wanafunzi[4] = "Wasike";
+
+        output = wanafunzi[0]+"\n"+wanafunzi[1]+"\n"+wanafunzi[2]+"\n"+wanafunzi[3]+"\n"+wanafunzi[4];
+        System.out.println(output);
+
+        System.out.println(wanafunzi[4]);
+
+    }
+}
